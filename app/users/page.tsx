7cafe6d425fa9a7da4ae3189ab53337c3e26dd02
antonis-angelis <agelisadonis@gmail.com>
2024-03-23 20:48:04 +0200

    added a dynamic users page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,3 +1,24 @@
-export default function Users() {
-  return <div>Users</div>;
+type User = {
+  id: number;
+  name: string;
+};
+
+export default async function Users() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map(({ id, name }) => (
+          <li key={id}>{name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
