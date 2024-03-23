a10d0e98445da016818abb9e0ec0a81afdee6124
antonis-angelis <agelisadonis@gmail.com>
2024-03-23 20:47:33 +0200

    render new components in root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import ProductCard from "./components/ProductCard/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
       <Link href="/users">Users</Link>
+      <ProductCard />
     </main>
   );
 }
