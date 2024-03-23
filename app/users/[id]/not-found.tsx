26eb5e625490ad6542fcb4172053901c8f5ed0ae
antonis-angelis <agelisadonis@gmail.com>
2024-03-24 01:07:22 +0200

    not found files

---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,3 @@
+export default function UserNotFound() {
+  return <h1>This user doesn't exist</h1>;
+}
--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,7 +1,11 @@
+import { notFound } from "next/navigation";
+
 type UserId = {
-  id: string;
+  id: number;
 };
 
 export default function UserDetails({ params }: { params: UserId }) {
+  if (params.id > 10) notFound();
+
   return <div>{params.id}</div>;
 }
