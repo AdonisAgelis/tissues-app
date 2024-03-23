0b0c048679c5802f361248b926891e4d9a91e3ae
antonis-angelis <agelisadonis@gmail.com>
2024-03-23 21:13:56 +0200

    extracted table to a new component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,35 +1,10 @@
-type User = {
-  id: number;
-  name: string;
-  email: string;
-};
+import UserTable from "./UserTable";
 
 export default async function Users() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    cache: "no-store",
-  });
-
-  const users: User[] = await res.json();
-
   return (
     <>
       <h1>Users</h1>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(({ id, name, email }) => (
-            <tr key={id}>
-              <td>{name}</td>
-              <td>{email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable />;
     </>
   );
 }
