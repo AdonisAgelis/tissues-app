d082fbcd959b27ed41bb14ffaa917eb14fcbc028
antonis-angelis <agelisadonis@gmail.com>
2024-03-25 19:35:27 +0200

    creating users dummy endpoints

---
+++ app/api/users/route.tsx
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(request: NextRequest) {
+  return NextResponse.json([
+    {
+      id: 1,
+      name: "Leanne Graham",
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+    },
+    {
+      id: 3,
+      name: "Clementine Bauch",
+    },
+    {
+      id: 4,
+      name: "Patricia Lebsack",
+    },
+    {
+      id: 5,
+      name: "Chelsey Dietrich",
+    },
+  ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  if (!body.name) {
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  }
+
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
