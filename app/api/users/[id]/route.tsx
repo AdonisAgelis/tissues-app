d5e417a9175a99fece60c834bdc516aeb252cb3d
antonis-angelis <agelisadonis@gmail.com>
2024-03-25 19:41:17 +0200

    dummy PUT request

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,11 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(
-  request: NextRequest,
-  { params }: { params: { id: number } }
-) {
+type UserId = {
+  params: { id: number };
+};
+
+export function GET(request: NextRequest, { params }: UserId) {
   if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
 
   return NextResponse.json({ id: 1, name: "Leanne Graham" });
+}
+
+export async function PUT(request: NextRequest, { params }: UserId) {
+  const body = await request.json();
+
+  if (!body.name) {
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  }
+
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: body.name });
 }
